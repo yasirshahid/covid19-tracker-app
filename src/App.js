9aa8570cc048dc8a1778676d688fddf5787ec7d8
yasirshahid <66242488+yasirshahid@users.noreply.github.com>
2020-07-09 14:38:05 +0500

    added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import InfoPanel from './components/InfoPanel';
+import FootNav from './components/FootNav';
 
 function App() {
   return (
     <div>
       <NavBar/>
+      <FootNav/>
       <InfoPanel/>
     </div>
   );
